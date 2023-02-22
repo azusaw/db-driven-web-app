9be0d181218e01122cc4eb364e9ca2228fcd8433
azusaw <azusa0304@gmail.com>
2023-02-22 17:04:01 +0000

    read all data from db

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    conn = sqlite3.connect("./data/earthquake_data.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("select * from earthquakes")
+    rows = cur.fetchall()
+    return render_template('index.html', rows=rows)
 
 if __name__ == "__main__":
     app.run(debug=True)
