0e3ce6e23a0b646a36981694e530a730f0d2a79c
azusaw <azusa0304@gmail.com>
2023-02-22 20:03:43 +0000

    separate all page from index

--- app.py
+++ app.py
@@ -5,12 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/all')
+def all_earthquakes():
     conn = sqlite3.connect("./data/earthquake_data.db")
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     cur.execute("select e.time, e.latitude, e.longitude, e.depth, e.mag, e.magType,  e.place, e.type, e.status, s.name from earthquakes e left join sources s on e.net = s.id ")
     rows = cur.fetchall()
-    return render_template('index.html', rows=rows)
+    return render_template('all_earthquakes.html', rows=rows)
 
 if __name__ == "__main__":
     app.run(debug=True)
