720a276121af76e518f4e2728fa5f34ad520b66b
azusaw <azusa0304@gmail.com>
2023-02-22 20:05:16 +0000

    rename file

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def index():
     return render_template('index.html')
 
 @app.route('/all')
-def all_earthquakes():
+def all():
     conn = sqlite3.connect("./data/earthquake_data.db")
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     cur.execute("select e.time, e.latitude, e.longitude, e.depth, e.mag, e.magType,  e.place, e.type, e.status, s.name from earthquakes e left join sources s on e.net = s.id ")
     rows = cur.fetchall()
-    return render_template('all_earthquakes.html', rows=rows)
+    return render_template('all.html', rows=rows)
 
 if __name__ == "__main__":
     app.run(debug=True)
