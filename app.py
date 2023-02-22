8851b31f83c7d8b212ab838185f34efd002a6923
azusaw <azusa0304@gmail.com>
2023-02-22 17:12:45 +0000

    edit sql to join two table: earthquakes and sources

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
     conn = sqlite3.connect("./data/earthquake_data.db")
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-    cur.execute("select * from earthquakes")
+    cur.execute("select e.time, e.latitude, e.longitude, e.depth, e.mag, e.magType,  e.place, e.type, e.status, s.name from earthquakes e left join sources s on e.net = s.id ")
     rows = cur.fetchall()
     return render_template('index.html', rows=rows)
 
