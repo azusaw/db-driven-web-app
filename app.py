c83b51ce416a606b690d520a45c7f1f2193c3bbb
azusaw <azusa0304@gmail.com>
2023-02-23 22:34:35 +0000

    rename page html

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ db = database.Database()
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home_page.html")
 
 
 @app.route("/all")
 def all():
     rows, cnt = db.select()
-    return render_template("all.html", rows=rows, cnt=cnt)
+    return render_template("all_page.html", rows=rows, cnt=cnt)
 
 
 @app.route("/search", methods=["GET", "POST"])
@@ -27,7 +27,7 @@ def search():
     mag_types = db.select_mag_types()
     if request.method == "POST":
         rows, cnt = db.select(request.form.to_dict(flat=True))
-    return render_template("search.html", rows=rows, cnt=cnt, sources=sources, mag_types=mag_types)
+    return render_template("search_page.html", rows=rows, cnt=cnt, sources=sources, mag_types=mag_types)
 
 
 @app.route("/ranking")
@@ -35,13 +35,13 @@ def ranking():
     mag_rows = db.select_mag_top3()
     depth_rows = db.select_depth_top3()
     country_rows = db.select_country_top5()
-    return render_template("ranking.html", mag_rows=mag_rows, depth_rows=depth_rows, country_rows=country_rows)
+    return render_template("ranking_page.html", mag_rows=mag_rows, depth_rows=depth_rows, country_rows=country_rows)
 
 
 @app.route('/magnitude_type')
 def magnitude_type():
     rows = db.select_mag_types()
-    return render_template("magnitude_type.html", rows=rows)
+    return render_template("magnitude_type_page.html", rows=rows)
 
 
 if __name__ == "__main__":
