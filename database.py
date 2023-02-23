322704c13850f572506c661a88eac517a536302d
azusaw <azusa0304@gmail.com>
2023-02-23 11:01:05 +0000

    add coments

--- database.py
+++ database.py
@@ -8,6 +8,7 @@ class Database:
     def __init__(self):
         pass
 
+    # Execute SELECT query with given conditions
     def select(self, where=""):
         print(where)
         conn = sqlite3.connect(self.dbname)
@@ -18,13 +19,20 @@ class Database:
             "from earthquakes e left join sources s on e.net = s.id "
             + where
         )
+
+        # Convert to dict type because Sqlite3 Row type does not allow values update
         rows = [dict(row) for row in cur]
+
         return map(self.cleansing_data, rows)
 
+    # Clean original opensource data to make easy to understand
     @staticmethod
     def cleansing_data(row):
+        # Remove 'Z' which causes the ISO date format conversion error
         d = datetime.datetime.fromisoformat(row["time"].replace('Z', ''))
+
         row["time"] = d.strftime('%d/%m/%Y %H:%M:%S')
         row["depth"] = format(float(row["depth"]), '.2f')
         row["mag"] = format(float(row["mag"]), '.2f')
+
         return row
