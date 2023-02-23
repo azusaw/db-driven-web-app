37d5ff43814c5cbe86acbbe49d8f0c7b5212a0a1
azusaw <azusa0304@gmail.com>
2023-02-23 02:41:14 +0000

    create data cleansing function

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlite3
 
 
@@ -13,8 +14,17 @@ class Database:
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
         cur.execute(
-            "select e.time, e.latitude, e.longitude, e.depth, e.mag, e.magType, e.place, e.type, e.status, s.name " +
+            "select e.time, e.depth, e.mag, e.magType, e.place, s.name " +
             "from earthquakes e left join sources s on e.net = s.id "
             + where
         )
-        return cur.fetchall()
+        rows = [dict(row) for row in cur]
+        return map(self.cleansing_data, rows)
+
+    @staticmethod
+    def cleansing_data(row):
+        d = datetime.datetime.fromisoformat(row["time"].replace('Z', ''))
+        row["time"] = d.strftime('%d/%m/%Y %H:%M:%S')
+        row["depth"] = format(float(row["depth"]), '.2f')
+        row["mag"] = format(float(row["mag"]), '.2f')
+        return row
